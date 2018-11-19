a9f733707e1845fbb9434c610e1fa0b00c79af37
Abel Rojas Bueno <801219@ACDM.DS.SAIT.CA>
2018-11-19 16:42:28 -0700

    Create header
    First version of header.

--- index.php
+++ index.php
@@ -1,7 +1,40 @@
-<?php
+<!DOCTYPE html>
+<html>
+<title>Travel Experts</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
+.w3-bar-block .w3-bar-item {padding:20px}
+</style>
+<script>
+// Script to open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
 
-// Here is to put the info
-echo ("Hello World");
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
+</script>
 
+<!-- Sidebar (hidden by default) -->
+<nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left"
+style="display:none;z-index:2;width:40%;min-width:300px" id="mySidebar">
+  <a href="javascript:void(0)" onclick="w3_close()"
+  class="w3-bar-item w3-button">Close Menu</a>
+  <a href="#packages" onclick="w3_close()" class="w3-bar-item w3-button">Travel packages</a>
+  <a href="register.php" onclick="w3_close()" class="w3-bar-item w3-button">Register</a>
+  <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button">About</a>
+</nav>
 
-?>
+<!-- Top menu -->
+<div class="w3-top">
+  <div class="w3-white w3-xlarge" style="max-width:1200px;margin:auto">
+    <div class="w3-button w3-padding-16 w3-left" onclick="w3_open()">☰</div>
+    <a href="contact.php"><div class="w3-right w3-padding-16">Contact us</div></a>
+    <div class="w3-center w3-padding-16"><strong>Travel Experts</strong></div>
+  </div>
+</div>
