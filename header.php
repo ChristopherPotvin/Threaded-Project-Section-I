70ea1f88e48b4ff1e8974d9aa788b38550f639d8
ChristopherPotvin <chrispotvin4@gmail.com>
2018-11-28 13:15:56 -0700

    Update header.php

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
       <div class="w3-button w3-padding-16 w3-left" onclick="w3_open()">☰</div>
       <div class="w3-center w3-padding-16"><strong>Travel Experts</strong></div>
       <!-- weather widget start -->
-      <div class="w3-right w3-top" a target="_blank" href="https://www.booked.net/weather/calgary-30502"><img src="https://w.bookcdn.com/weather/picture/32_30502_1_1_34495e_250_2c3e50_ffffff_ffffff_1_2071c9_ffffff_0_6.png?scode=2&domid=569&anc_id=70976" alt="booked.net"/></a></div><!-- weather widget end -->
+      <div class="w3-left w3-top" a target="_blank" href="https://www.booked.net/weather/calgary-30502"><img src="https://w.bookcdn.com/weather/picture/32_30502_1_1_34495e_250_2c3e50_ffffff_ffffff_1_2071c9_ffffff_0_6.png?scode=2&domid=569&anc_id=70976" alt="booked.net"/></a></div><!-- weather widget end -->
     </div>
   </div>
 
