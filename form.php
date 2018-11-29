b7f042273cd8579c6639dedc53ad877ba111606c
acpoosd <alex.procyk@edu.sait.ca>
2018-11-29 09:55:37 -0700

    Fixes of headers

--- form.php
+++ form.php
@@ -1,33 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
 
     <title>Travel Experts</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
-    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
-    <script src="js/functions.js"></script>
+	<?php
+      include "head-links.php";
+    ?>
 	
-    body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
-    .w3-bar-block .w3-bar-item {padding:20px}
-	
-	footer {font-family:Impact; background-color: silver;}	
-		
-	h1 {color: white;}
-		
-	h4 {font-family: Arial; font-style: italic;}
-		
-	h6 {font-family: Arial; color: #8B0000;}	
-</style>
 </head>
 
 <header>
 	<?php
-		require("header.php");
+		include("header.php");
 	?>
 </header>
 
