577973b2f5c68b75e5a11968d5c57a69e8cbfe3a
Abel Rojas Bueno <801219@ACDM.DS.SAIT.CA>
2018-11-29 15:22:22 -0700

    Footer latest

--- footer.php
+++ footer.php
@@ -1,56 +1,31 @@
-<head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
-
-</head>
-
 <style>
-	body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
-
-	footer {
-		font-family: "Karma", sans-serif;
-	}
-
-	h1 {
-        font-weight: bold;
-        color: black;
-	}
-
-	h4 {
-        color: grey;
-	}
-
-	h6 {
-        color: black;
-	}
   #widget {
-    position: relative;
+    position: sticky;
     float: left;
-    z-index:2;
+    z-index:1;
   }
 </style>
 
-</head>
+<hr>
 
-<body>
+<footer>
 
-   <h1 style="text-align: center; font-size: 21px;">The Travel Experts</h1>
-   <h4 style="text-align: center; font-size: 17px">More than just a good time...</h4>
-   <h6 style="text-align: center; font-size: 11px">Images by: Pexels.com<br>Icons by: Icons8.com</h6>
-   <h6 style="text-align: center; font-size: 11px">Weather Widget by: ibooked.ca<br></h6>
-   <h6 style="text-align: center; font-size: 11px">&copy 2018 The Travel Experts <sup>TM</sup></h6>
+   <h1 style="text-align: center; font-size: 21px; font-weight: bold; color: black;">The Travel Experts</h1>
+   <h4 style="text-align: center; font-size: 17px; color: grey;">More than just a good time...</h4>
+   <h6 style="text-align: center; font-size: 11px; color: black;">Images by: Pexels.com<br>Icons by: Icons8.com</h6>
+   <h6 style="text-align: center; font-size: 11px; color: black;">Weather Widget by: ibooked.ca<br></h6>
+   <h6 style="text-align: center; font-size: 11px; color: black;">&copy 2018 The Travel Experts <sup>TM</sup></h6>
 
-
-   <div class="footer">
      <a href="https://www.facebook.com/" target="_blank">
      <img id="icon" src="images/fb.png" style="float: right";></a>
      <a href="https://www.instagram.com/" target="_blank" >
      <img id="icon" src="images/insta.png" style ="float: right";></a>
      <a href="https://twitter.com/" target="_blank">
      <img id="icon" src="images/twitter.png" style="float: right";></a>
-     <a id="widget" target="_blank" href="https://www.booked.net/weather/calgary-30502">
-       <img src="https://w.bookcdn.com/weather/picture/32_30502_1_1_34495e_250_2c3e50_ffffff_ffffff_1_2071c9_ffffff_0_6.png?scode=2&domid=569&anc_id=70976"
-       alt="booked.net"/></a><!-- weather widget end -->
-    </div>
 
-</body>
+
+    <a id="widget" target="_blank" href="https://www.booked.net/weather/calgary-30502">
+      <img src="https://w.bookcdn.com/weather/picture/32_30502_1_1_34495e_250_2c3e50_ffffff_ffffff_1_2071c9_ffffff_0_6.png?scode=2&domid=569&anc_id=70976"
+      alt="booked.net"/></a><!-- weather widget end -->
+
 </footer>
