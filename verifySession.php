4d1fd8caa08388d17844a81ed5314adb03cfeefb
Jonathan Pirca Chang <730134@ACDM.DS.SAIT.CA>
2018-12-02 06:44:08 -0700

    Rest of the pages commented

--- verifySession.php
+++ verifySession.php
@@ -1,9 +1,18 @@
 <?php
+/*
+	Author: Jonathan Pirca
+	Date: Nov 30th, 2018; 
+    Details: Check if the user is already logged id.
+*/
+?>
+<?php
 	session_cache_expire(10);
 	session_start();
 	
+	// Get the package ID and set up the session variable
 	$_SESSION["packages"] = $_REQUEST["pckId"];
 	
+	// Check if user was logged before, then user does not have to loggin again
 	if (!isset($_SESSION["logged"]))
 	{
 		header("location: login.php");
